e16853d754f1cc18fb93fa969e14213806f3f17c
rentianyu <1782898491@qq.com>
2020-07-17 14:19:51 +0800

    auto-push

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -2,13 +2,13 @@
 if [ $(whoami) == root ]
 then
     echo 1.更新软件包
-    apt update -y && apt upgrade -y && apt autoremove -y
+    apt update -y ; apt upgrade -y ; apt autoremove -y
 
     echo 2.安装应用包
-    apt install -y vim git unrar fish nmap python jq openssh nodejs
+    apt install -y vim git unrar fish nmap python jq nodejs
 
     echo 3.设置默认shell为fish并清空欢迎语
-    chsh -s $(which fish) && fish -c "set -U fish_greeting"
+    chsh -s $(which fish) ; fish -c "set -U fish_greeting"
 
     echo 4.定义一些快捷命令
     echo "apti aptr aa lsa .. nmap1 nmap6"
@@ -20,11 +20,11 @@ then
 alias apti='apt -y install'
 alias aptr='apt -y remove'
 alias apts='apt search'
-alias aa='apt update -y && apt upgrade -y && apt autoremove -y && do-release-upgrade'
+alias aa='apt update -y ; apt upgrade -y ; apt autoremove -y ; do-release-upgrade'
 
 alias lsa='ls -a'
-alias ..='cd ..&& ls -a'
-alias gitp='git add . && git commit -m "auto-push" && git push && echo push成功'
+alias ..='cd ..; ls -a'
+alias gitp='git add . ; git commit -m "auto-push" ; git push ; echo push成功'
 
 alias nmap1='nmap 127.0.0.1'
 
@@ -35,7 +35,7 @@ alias nmap1='nmap 127.0.0.1'
     git config --global user.email "1782898491@qq.com"
     
     echo 6.Ubuntu设置终端为中文
-apt install -y language-pack-zh-hans && locale-gen zh_CN.UTF-8 &&
+apt install -y language-pack-zh-hans ; locale-gen zh_CN.UTF-8 ;
     echo '
 LANG="zh_CN.UTF-8"
 LANGUAGE="zh_CN:zh:en_US:en"
@@ -60,7 +60,7 @@ LC_ALL=zh_CN.UTF-8' > /etc/default/locale
         echo 'v2ray脚本'
         wget -O v2ray.sh https://git.io/v2ray.sh | chmod +x v2ray.sh
         echo 'BBR加速脚本'
-        wget -O BBR.sh https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh && chmod +x BBR.sh
+        wget -O BBR.sh https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh ; chmod +x BBR.sh
 
     echo 8.安装并配置oy my fish
     curl -L https://get.oh-my.fish | fish
