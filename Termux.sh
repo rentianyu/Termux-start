d311b15b3a40e24495c71063ba65f77d251a35e1
rentianyu <1782898491@qq.com>
2020-09-04 17:32:30 +0800

    auto-push

--- Termux.sh
+++ Termux.sh
@@ -76,5 +76,5 @@ git config --global user.email "1782898491@qq.com"
 echo 9.安装并配置oy my fish
 termux-clipboard-set "omf install ays"
 curl -L https://get.oh-my.fish > omf.fish
-echo exit > omf.fish
+echo exit >> omf.fish
 fish omf.fish
